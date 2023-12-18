e167c4fb226d4959cc0d0db168cf26548eae3bde
SheepSuSu <148197020+SheepSuSu@users.noreply.github.com>
2023-12-19 06:32:26 +0800

    Update BankDAOImplTest.java

--- src/cake/wx/shopping/test/BankDAOImplTest.java
+++ src/cake/wx/shopping/test/BankDAOImplTest.java
@@ -9,9 +9,9 @@ public class BankDAOImplTest {
 
 		try {
 
-			System.out.println((Boolean)DAOFactory.getBankDAOInstance().select("aliqi", "766"));
-			DAOFactory.getBankDAOInstance().update("aliqi", "766",789);
-			System.out.println(String.valueOf(DAOFactory.getBankDAOInstance().select("aliqi")));
+			System.out.println((Boolean)DAOFactory.getBankDAOInstance().select("test_user", "886"));
+			DAOFactory.getBankDAOInstance().update("test_user", "886",789);
+			System.out.println(String.valueOf(DAOFactory.getBankDAOInstance().select("test_user")));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -19,3 +19,4 @@ public class BankDAOImplTest {
 	}
 
 }
+
