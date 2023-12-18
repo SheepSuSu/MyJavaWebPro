5593cfad68e44dbf8cc6ed994789b2d9f9677969
SheepSuSu <148197020+SheepSuSu@users.noreply.github.com>
2023-12-19 06:35:44 +0800

    Update UserFormOBeanTest.java

--- src/cake/wx/shopping/test/UserFormOBeanTest.java
+++ src/cake/wx/shopping/test/UserFormOBeanTest.java
@@ -6,12 +6,7 @@ import java.util.List;
 import cake.wx.shopping.vo.Product;
 import cake.wx.shopping.busbean.*;
 
-/**
- * 
- * @author 陈志军测试
- * 
- *
- */
+
 public class UserFormOBeanTest {
 	
 
@@ -19,23 +14,23 @@ public class UserFormOBeanTest {
 		List<Product> list=new ArrayList<Product>();
 		Product p1=new Product();
 		p1.setPid("1");
-		p1.setPname("ipod");
+		p1.setPname("cake1");
 		p1.setPcount(5);
 		p1.setPrice(10);
 		Product p2=new Product();
 		p2.setPid("2");
-		p2.setPname("电脑");
+		p2.setPname("cake2");
 		p2.setPcount(5);
 		p2.setPrice(20);
 		Product p3=new Product();
 		p3.setPid("3");
-		p3.setPname("ibm笔记本");
+		p3.setPname("cake3");
 		p3.setPcount(5);
 		p3.setPrice(30);
 		list.add(p1);
 		list.add(p2);
 		list.add(p3);
-		UserFormOBean.buyProcess("丹丹", list);
+		UserFormOBean.buyProcess("bbb", list);
 		
 	}
 
